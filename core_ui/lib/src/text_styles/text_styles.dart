9bb430a1c1ac3b1589341e2376d17a51364375a9
olesyalosik <losiko2004@gmail.com>
2023-08-07 04:17:30 +0300

    Add text styles

---
+++ core_ui/lib/src/text_styles/text_styles.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/cupertino.dart';
+
+class TextStyles {
+  static const TextStyle comfortaa_bold_24 = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Comfortaa',
+  );
+
+  static const TextStyle comfortaa_bold_16 = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Comfortaa',
+  );
+  static const TextStyle comfortaa_bold_14 = TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Comfortaa',
+  );
+  static const TextStyle comfortaa_bold_12 = TextStyle(
+    fontSize: 12.0,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Comfortaa',
+  );
+  static const TextStyle comfortaa_regular_24 = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Comfortaa',
+  );
+
+  static const TextStyle comfortaa_regular_16 = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Comfortaa',
+  );
+  static const TextStyle comfortaa_regular_14 = TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Comfortaa',
+  );
+  static const TextStyle comfortaa_regular_12 = TextStyle(
+    fontSize: 12.0,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Comfortaa',
+  );
+  static const TextStyle comfortaa_light_24 = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.w300,
+    fontFamily: 'Comfortaa',
+  );
+
+  static const TextStyle comfortaa_light_16 = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w300,
+    fontFamily: 'Comfortaa',
+  );
+  static const TextStyle comfortaa_light_14 = TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w300,
+    fontFamily: 'Comfortaa',
+  );
+  static const TextStyle comfortaa_light_12 = TextStyle(
+    fontSize: 12.0,
+    fontWeight: FontWeight.w300,
+    fontFamily: 'Comfortaa',
+  );
+  static const TextStyle comfortaa_medium_24 = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Comfortaa',
+  );
+
+  static const TextStyle comfortaa_medium_16 = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Comfortaa',
+  );
+  static const TextStyle comfortaa_medium_14 = TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Comfortaa',
+  );
+  static const TextStyle comfortaa_medium_12 = TextStyle(
+    fontSize: 12.0,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Comfortaa',
+  );
+  static const TextStyle comfortaa_semibold_24 = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Comfortaa',
+  );
+
+  static const TextStyle comfortaa_semibold_16 = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Comfortaa',
+  );
+  static const TextStyle comfortaa_semibold_14 = TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Comfortaa',
+  );
+  static const TextStyle comfortaa_semibold_12 = TextStyle(
+    fontSize: 12.0,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Comfortaa',
+  );
+}
